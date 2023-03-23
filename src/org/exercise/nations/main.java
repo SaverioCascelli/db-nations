cf275004c86f73b034d2dbcaf59227808115f7ba
SaverioCascelli <saverio.cascelli@gmail.com>
2023-03-23 15:02:37 +0100

    milestone 3

--- src/org/exercise/nations/main.java
+++ src/org/exercise/nations/main.java
@@ -1,22 +1,30 @@
 package org.exercise.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/db_nat ions";
+        String url = "jdbc:mysql://localhost:3306/db_nations";
         String user = "root";
         String password = "root";
 
+        System.out.println("Inserisci una stringa di ricerca per nazione");
+        Scanner scan = new Scanner(System.in);
+        String filter = scan.nextLine();
+        scan.close();
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             String sql = """
                     select c.name as country_name , c.country_id , r.name as region_name ,c2.name as continent_name from countries c
                     join regions r on r.region_id = c.region_id 
                     join continents c2 on r.continent_id = r.region_id 
+                    where c.name like ?
                     order by c.name 
                             """;
             try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setString(1, "%" + filter + "%");
+
                 try (ResultSet rs = ps.executeQuery()) {
                     while (rs.next()) {
                         String countryName = rs.getString(1);
